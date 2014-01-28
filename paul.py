893c442720d8d26563ef7d3ebe049eca23d4394f
Kyle <kyle@wellandlighthouse.com>
2014-01-28 18:28:54 -0500

    prelim plan

---
+++ paul.py
@@ -0,0 +1,19 @@
+#v0.1 -- twitter to user
+#make call to twitter REST API
+#return URLs and parse them into a list of sorts
+#write out a CSV // something that will get updated (append the CSV?)
+
+#v0.2
+#give the user the ability to annotate each entry
+
+#v0.3
+#give the user the ability to tag things using hashtags
+
+#v0.4
+#connect to tumblr
+
+#v2.0
+#wordpress widget
+
+#v3.0
+#website plugin
