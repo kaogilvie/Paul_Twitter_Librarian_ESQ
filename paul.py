494fe16060b4189dc5637e7a47ed6c22e8954236
Kyle <kyle@wellandlighthouse.com>
2014-01-29 08:59:42 -0500

    halfway there with OAuth. might currently be underwater with competing libraries (requests and twython)

--- paul.py
+++ paul.py
@@ -1,6 +1,40 @@
+import requests
+from requests_oauthlib import OAuth1
+from twython import Twython
+from fortknox import PTLE
+
+
 #v0.1 -- twitter to user
 #make call to twitter REST API
+
+username = 'thecliffsodover'
+get_request = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+username+"&count=10"
+#callback = 
+
+twitter = Twython(PTLE['APP_KEY'], PTLE['APP_SECRET'])
+auth = twitter.get_authentication_tokens()
+#need callback URL for web application
+#auth = twitter.get_authentication_token(callback_url=callback)
+
+OAUTH_TOKEN = auth['oauth_token']
+OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+#should store these in a session variable in Django when you migrate to web
+
+#(should redirect here??) 
+verify_creds = auth['auth_url']
+
+auth = OAuth1(PTLE['APP_KEY'], PTLE['APP_SECRET'], OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+r = requests.get(get_request, auth=auth)
+print r.json()
+
 #return URLs and parse them into a list of sorts
+
+for i in response:
+	for k in i['urls']:
+		grab_url = k['expanded_url']
+		print grab_url
+
 #write out a CSV // something that will get updated (append the CSV?)
 
 #v0.2
