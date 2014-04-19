f45ccfe38430c3e96abc2c94febb7502f97f3322
Kyle <kyle@wellandlighthouse.com>
2014-04-19 14:24:02 -0400

    working response via flask! need to parse and make the response look pretty

--- paul.py
+++ paul.py
@@ -2,43 +2,44 @@ from twython import Twython
 from fortknox import PTLE
 import csv
 
-#via ftknox
-app_key = PTLE['APP_KEY']
-app_secret = PTLE['APP_SECRET']
+def get_timeline(app_key, app_secret, username):
+	#prompt for username
+	#username = raw_input('Username: ')
+	#archive_file = username+'.csv'
+	response_dict = {}
 
+	#try:
+	#	file = open(archive_file, 'r+b')
+	#	tweet_reader = csv.DictReader(file)
+	#	response_dict = tweet_reader.next()
+	#except IOError:
+	#	print 'No archive file. New tweet timeline will be created.'
 
-#prompt for username
-username = raw_input('Username: ')
-archive_file = username+'.csv'
-response_dict = {}
+	#v0.1 -- twitter to user vai OAuth2 -- put onto Django.
+	twitter = Twython(app_key, app_secret, oauth_version=2)
+	ACCESS_TOKEN = twitter.obtain_access_token()
+	twitter = Twython(app_key, access_token=ACCESS_TOKEN)
 
-try:
-	file = open(archive_file, 'r+b')
-	tweet_reader = csv.DictReader(file)
-	response_dict = tweet_reader.next()
-except IOError:
-	print 'No archive file. New tweet timeline will be created.'
-	pass
+	response = twitter.get_user_timeline(screen_name=username, count=1)
 
-#v0.1 -- twitter to user vai OAuth2 -- put onto Django.
-twitter = Twython(app_key, app_secret, oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
-twitter = Twython(app_key, access_token=ACCESS_TOKEN)
+	print response
 
-response = twitter.get_user_timeline(screen_name=username, count=1)
+	for i in response:
+		grab_url = False
+		date = i['created_at']
+		for k in i['entities']['urls']:
+			grab_url = k['expanded_url']
+		if grab_url is not False:
+			response_dict[date] = grab_url
+	keys = response_dict.keys()
 
-for i in response:
-	grab_url = False
-	date = i['created_at']
-	for k in i['entities']['urls']:
-		grab_url = k['expanded_url']
-	if grab_url is not False:
-		response_dict[date] = grab_url
-keys = response_dict.keys()
+	print keys
 
-tweet_writer = csv.DictWriter(open(archive_file, 'wb'), keys)
-tweet_writer.writeheader()
-tweet_writer.writerow(response_dict)
+	#tweet_writer = csv.DictWriter(open(archive_file, 'wb'), keys)
+	#tweet_writer.writeheader()
+	#tweet_writer.writerow(response_dict)
+
+	return response
 
 #v0.2
 #hoist onto the web using FLASK (?) -- return json response and print out
--- paul_flask.py
+++ paul_flask.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from fortknox import PTLE
+import paul
 
 app = Flask(__name__)
 
+#app variables
+app_key = PTLE['APP_KEY']
+app_secret = PTLE['APP_SECRET']
+
 @app.route("/")
-def paul():
+def paul_home():
 	return render_template('paul.html')
 
-@app.route("/annals")
+@app.route("/annals", methods=['GET', 'POST'])
 def annals():
-	return 'results page'
+	if request.method == 'POST':
+		username = request.form['username']
+		response = paul.get_timeline(app_key, app_secret, username)
+		return render_template('annals.html', response=response)
+	else:
+		return 'get request'
 
 if __name__ == '__main__':
 	app.debug = True
